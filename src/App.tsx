e0717c8a7b84297d274ad75a84cc78c651956152
Seu Nome <Seu Nome>
2025-08-01 10:28:49 -0300

    Usuário escolhe a operação de deposito ou saque.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import './App.css'
 import { ContaBancaria } from './models/ContaBancaria';
+import { useState } from 'react';
 
 function App() {
-  const conta = new ContaBancaria();
-  conta.depositar(100);
-  conta.sacar(40);
+  const [conta] = useState(new ContaBancaria());
+  const [valor, setValor] = useState(0);
+  const [saldo, setSaldo] = useState(conta.verSaldo());
 
-  return <p>Saldo disponível: {conta.verSaldo()}</p>;
+  const handleDepositar = () => {
+    conta.depositar(valor);
+    setSaldo(conta.verSaldo());
+  };
+
+  const handleSacar = () => {
+    conta.sacar(valor);
+    setSaldo(conta.verSaldo());
+  };
+
+  return (
+    <div>
+      <h2>Saldo disponível: {saldo}</h2>
+      <input
+        type="number"
+        value={valor}
+        onChange={e => setValor(Number(e.target.value))}
+        placeholder="Digite o valor"
+      />
+      <button onClick={handleDepositar}>Depositar</button>
+      <button onClick={handleSacar}>Sacar</button>  
+    </div>
+  );
 }
 
 export default App;
